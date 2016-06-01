aaf74533ee862ad0c29556811db50fa4c212ae3c
joyforza <lehungson007@gmail.com>
2016-06-01 07:38:07 +0700

    Update #4

---
+++ src/mathapp/Calculator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathapp;
+
+/**
+ *
+ * @author sonlh
+ */
+public class Calculator {
+    public int sum(int a, int b) {
+        return 0;
+    }
+    
+    public int subtract(int a, int b) {
+        return 0;
+    }
+    
+    public int divide(int a, int b) {
+        return 0;
+    }
+    public int mul(int a, int b) {
+        return 0;
+    }
+}
