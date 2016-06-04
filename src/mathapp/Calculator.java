cbb58cc385c03ab8f15cf819cda9170b47380258
joyforza <lehungson007@gmail.com>
2016-06-04 17:47:51 +0700

    Update #5

--- src/mathapp/Calculator.java
+++ src/mathapp/Calculator.java
@@ -11,17 +11,18 @@ package mathapp;
  */
 public class Calculator {
     public int sum(int a, int b) {
-        return 0;
+        
+        return a + b;
     }
     
     public int subtract(int a, int b) {
-        return 0;
+        return a - b;
     }
     
     public int divide(int a, int b) {
-        return 0;
+        return a / b;
     }
     public int mul(int a, int b) {
-        return 0;
+        return a * b;
     }
 }
