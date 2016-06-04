a8fc502e8935ced6b6773586beca9933994ccad5
joyforza <lehungson007@gmail.com>
2016-06-04 22:00:50 +0700

    Add files via upload
    Add 2 file class

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathapp;
+
+/**
+ *
+ * @author sonlh
+ */
+public class Calculator {
+    public int sum(int a, int b) {
+        
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public int divide(int a, int b) {
+        return a / b;
+    }
+    public int mul(int a, int b) {
+        return a * b;
+    }
+    
+    public int gcd(int a, int b) {
+        return 0;
+    }
+}
